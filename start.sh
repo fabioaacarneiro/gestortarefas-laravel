e63c881f6364ae339e84c33656b1428cd744a122
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-02-05 20:11:47 -0300

    atualizado arquivo start.sh para teste de deploy

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+# composer global require hirak/prestissimo
+# composer install --no-dev --working-dir=/var/www/html
 
-echo "Caching config..."
-php artisan config:cache
+composer install
+php artisan migrate
 
-echo "Caching routes..."
-php artisan route:cache
+# echo "Caching config..."
+# php artisan config:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+# echo "Caching routes..."
+# php artisan route:cache
+
+# echo "Running migrations..."
+# php artisan migrate --force
