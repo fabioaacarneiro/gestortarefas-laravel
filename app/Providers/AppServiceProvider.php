0e34edcf323b9d8ed5b1357dd4f4776cf341f282
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-02-05 17:42:46 -0300

    ajustado AppServiceProvider.php para forçar https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGe $url): void
+    public function boot(UrlGenerator $url): void
     {
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
