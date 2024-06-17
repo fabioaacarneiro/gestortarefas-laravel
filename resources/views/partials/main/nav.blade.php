6ee9e83c53c25bd1c502e9be0e148f56edc7410b
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-06-17 16:20:09 -0300

    feat(main-nav): adicionar responsividade no navbar
    navbar quando logado precisava de responsividade, foi corrigido
    a personalização do bootstrap para fornecer um menu de navegação
    suspenso e um botão humburger para melhorar a usabilidade em
    dispositivos móveis e telas pequenas.

--- resources/views/partials/main/nav.blade.php
+++ resources/views/partials/main/nav.blade.php
@@ -1,33 +1,58 @@
-<div class="bg-dark text-white mb-3 shadow">
-    <div class="contaner-fluid">
-        <div class="row align-items-center mx-4">
-            <div class="col col-md p-1">
-                <span><a href="{{ route('task.userhome') }}" class="btn btn-dark"><i
-                            class="bi bi-person"></i>{{ $user_name }}</a></span>
-                <span><a href="{{ url()->previous() }}"
-                        class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Voltar</a></span>
-                <span><a href="{{ route('task.show') }}"
-                        class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Tarefas</a></span>
-                <span><a href="{{ route('tasklist.show') }}"
-                        class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Listas</a></span>
+<div class="bg-dark text-white mb-3 shadow container-fluid">
+    {{-- <div class="container w-100"> --}}
+    <div class="navbar navbar-expand-lg navbar-dark row align-items-center">
+        <div class="col">
+            <a href="{{ route('task.userhome') }}" class="btn btn-dark"><i class="bi bi-person fs-3 me-1"></i>
+                {{ $user_name }}</a>
+        </div>
 
-            </div>
-            <div class="col col-md p-1">
-                <span class="text-success">Nível {{ $user_level }} - {{ $user_experience }}%</span>
-            </div>
-            {{-- <div class="col col-sm p-3 text-end">
-                <span><i class="bi bi-person"></i>{{ $user_name }}</span>
-            </div>
-            <div class="col col-auto">
-                <span class="opacity-50"><i class="bi bi-three-dots-vertical"></i></span>
-            </div> --}}
-            <div class="col col-auto">
-                <form action="{{ route('logout.submit') }}" method="post">
-                    @csrf
-                    <button type="submit" class="btn btn-outline-danger my-3"><i
-                            class="bi bi-box-arrow-right">Sair</i></button>
-                </form>
+        <div class="col-auto">
+            <span class="navbar-toggler shadow shadow-md btn-sm" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavMain" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <i class="bi bi-list fs-2"></i>
+            </span>
+        </div>
+
+
+        <div class="col-auto d-none d-sm-none d-md-none d-lg-block">
+            <span><a href="{{ url()->previous() }}"
+                    class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Voltar</a></span>
+            <span><a href="{{ route('task.show') }}"
+                    class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Tarefas</a></span>
+            <span><a href="{{ route('tasklist.show') }}"
+                    class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Listas</a></span>
+        </div>
+
+        <div class="col-auto">
+            <form action="{{ route('logout.submit') }}" method="post">
+                @csrf
+                <button type="submit" class="btn btn-outline-danger my-3"><i
+                        class="bi bi-box-arrow-right">Sair</i></button>
+            </form>
+        </div>
+
+
+    </div>
+    <div class="row my-0">
+        <div class="col d-flex justify-content-center">
+            <span class="text-success text-center">Nível {{ $user_level }} - {{ $user_experience }}%</span>
+        </div>
+    </div>
+    <div class="d-lg-none row mb-2">
+        <div class="col-sm-12">
+            <div class="collapse navbar-collapse" id="navbarNavMain">
+                <hr class="align-itens-center w-75 mx-auto">
+                <div class="d-flex justify-content-evenly">
+                    <span><a href="{{ url()->previous() }}"
+                            class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Voltar</a></span>
+                    <span><a href="{{ route('task.show') }}"
+                            class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Tarefas</a></span>
+                    <span><a href="{{ route('tasklist.show') }}"
+                            class="btn btn-dark link-underline link-underline-opacity-0 link-opacity-50-hover">Listas</a></span>
+                </div>
             </div>
         </div>
     </div>
+    {{-- </div> --}}
 </div>
