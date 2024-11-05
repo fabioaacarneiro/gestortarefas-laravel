cbe5712b6274cbb1ff687ee8f145f4150b14abe7
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-11-05 10:09:43 -0300

    feat: migration para adicionar colunas para gerir tempo da tarefa

---
+++ database/migrations/2024_11_05_130338_add_elapsed_time_to_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->integer('elapsed_time')->unsigned()->default(0);
+            $table->boolean('is_running')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('elapsed_time');
+            $table->dropColumn('is_running');
+        });
+    }
+};
