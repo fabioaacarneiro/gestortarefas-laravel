33ba0ee5a2ddf0838e6c844ae9a115e1c8b2f7b6
Fabio Carneiro <fabioaacarneiro@gmail.com>
2024-11-05 19:40:02 -0300

    fix: apenas um cronometro roda por vez

--- resources/views/partials/tasklist/modal_stopwatch_task.blade.php
+++ resources/views/partials/tasklist/modal_stopwatch_task.blade.php
@@ -9,11 +9,11 @@
                 <div class="d-flex justify-content-center">
                     <p id="timerDisplay_{{ $modal_id }}" class="fs-2">00:00</p> <!-- Exibição do cronômetro -->
                 </div>
+                <div id="messageContainer_{{ $modal_id }}"></div>
                 <div class="d-flex justify-content-center" role="group" aria-label="Timer Controls">
                     <div class="btn-group">
                         <button id="playButton_{{ $modal_id }}" class="btn btn-success btn-lg"><i class="fas fa-play"></i></button>
-                        <button id="pauseButton_{{ $modal_id }}" class="btn btn-warning btn-lg" disabled><i class="fas fa-pause"></i></button>
-                        <button id="stopButton_{{ $modal_id }}" class="btn btn-danger btn-lg" disabled><i class="fas fa-stop"></i></button>
+                        <button id="pauseButton_{{ $modal_id }}" class="btn btn-danger btn-lg" disabled><i class="fas fa-pause"></i></button>
                     </div>
                 </div>
             </div>
@@ -21,9 +21,28 @@
     </div>
 </div>
 
+<!-- Modal de Aviso: Cronômetro Já Em Execução -->
+<div class="modal fade" id="modalAlert" tabindex="-1" aria-labelledby="modalAlertLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalAlertLabel">Atenção!</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Já existe um cronômetro em execução. Pause ou pare o cronômetro atual antes de iniciar outro.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <script>
     // A lógica agora será carregada apenas quando o modal for mostrado
-    document.getElementById('modalTaskChronometer_{{ $task['id'] }}_{{ $list_id }}').addEventListener('show.bs.modal', function () {
+    document.getElementById("{{ $modal_id }}").addEventListener('show.bs.modal', function () {
         const taskId = "{{ $task['id'] }}";
         const url = `{{ route('getTaskTime') }}?task_id=${taskId}`;
 
@@ -51,7 +70,6 @@
         let runningTaskKey = `runningTaskId_${modalTaskId}`;
         const playButton = document.getElementById(`playButton_${modalTaskId}`);
         const pauseButton = document.getElementById(`pauseButton_${modalTaskId}`);
-        const stopButton = document.getElementById(`stopButton_${modalTaskId}`);
 
         let timer;
         let isRunning = false;
@@ -111,15 +129,51 @@
             localStorage.setItem(`isRunning_${modalTaskId}`, isRunning);
         }
 
+        // Função para verificar se algum outro cronômetro está em execução
+        function existAnotherRunningTask () {
+            const currentRunningTask = localStorage.getItem("running_task");
+            console.log(`currentRunningTask: ${currentRunningTask}`)
+            console.log(`currentTask: ${modalTaskId}`)
+            if (currentRunningTask && currentRunningTask !== modalTaskId) {
+                return true
+            }
+            return false
+        }
+
         // Função para iniciar o cronômetro
         function startTimer() {
             if (!isRunning) {
-                const currentRunningTask = localStorage.getItem(runningTaskKey);
-                if (currentRunningTask && currentRunningTask !== modalTaskId) {
-                    pauseTimer();
+                // Verifica se já existe outro cronômetro em execução
+            
+                if (existAnotherRunningTask()) {
+
+                    console.log(existAnotherRunningTask())
+
+                    // Se outro cronômetro está rodando, exibe um alerta
+                    let timeLess = 3;
+
+                    // Exibe a mensagem com o tempo restante
+                    const messageContainer = document.getElementById("messageContainer_{{ $modal_id }}");
+                    messageContainer.innerHTML = `<p id="messageError_{{ $modal_id }}" class="text-center text-danger">Já existe uma tarefa em progresso! - ${timeLess}</p>`;
+
+                    // Intervalo para reduzir o timeLess
+                    let countDownMessageTimer = setInterval(function() {
+                        timeLess--;  // Decrementa o tempo
+                        // Atualiza a mensagem com o tempo restante
+                        messageContainer.innerHTML = `<p class="text-center text-danger">Já existe uma tarefa em progresso! - ${timeLess}</p>`;
+
+                        // Se o contador chegar a 0, para o intervalo e limpa a mensagem
+                        if (timeLess <= 0) {
+                            clearInterval(countDownMessageTimer); // Para o contador
+                            messageContainer.innerHTML = ""; // Limpa a mensagem
+                        }
+                    }, 1000);
+                    
+                    return
                 }
 
-                localStorage.setItem(runningTaskKey, modalTaskId);
+                // Marca este cronômetro como o em execução
+                localStorage.setItem("running_task", modalTaskId);
                 isRunning = true;
                 startTime = Date.now() - elapsedTime * 1000;
                 timer = setInterval(function() {
@@ -130,7 +184,6 @@
 
                 playButton.disabled = true;
                 pauseButton.disabled = false;
-                stopButton.disabled = false;
             }
         }
 
@@ -141,38 +194,21 @@
                 isRunning = false;
                 playButton.disabled = false;
                 pauseButton.disabled = true;
-                stopButton.disabled = false;
                 saveState();
-                localStorage.setItem(runningTaskKey, '');
+                localStorage.setItem("running_task", ''); // Remove o cronômetro em execução
             }
-        }
-
-        // Função para parar o cronômetro
-        function stopTimer() {
-            if (isRunning) {
-                clearInterval(timer);
-            }
-            elapsedTime = 0;
-            updateDisplay();
-            playButton.disabled = false;
-            pauseButton.disabled = true;
-            stopButton.disabled = true;
-            saveState();
-            localStorage.setItem(runningTaskKey, '');
         }
 
         // Adiciona os eventos aos botões
         playButton.addEventListener("click", startTimer);
         pauseButton.addEventListener("click", pauseTimer);
-        stopButton.addEventListener("click", stopTimer);
 
         // Inicializa o display do cronômetro
         updateDisplay();
 
         // Quando o modal for fechado, limpamos a lógica
         document.getElementById('modalTaskChronometer_{{ $task['id'] }}_{{ $list_id }}').addEventListener('hidden.bs.modal', function () {
-            clearInterval(timer);
+            clearInterval(timer);  // Remove qualquer intervalo em execução
         });
     });
-
 </script>
